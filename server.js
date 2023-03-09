261caa1b37521e15766ecbf4d7eb0b72a1ad84ed
Victor Manuel Torrico Diaz <99618387+Sugar2334@users.noreply.github.com>
2023-03-09 20:46:15 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,66 @@
+import express from 'express'
+import  handlebars  from 'express-handlebars'
+
+import productRouter from './src/routes/products.js'
+import cartRouter from './src/routes/cart.js'
+import homeRouter from './src/routes/home.js'
+import realTimeRouter from './src/routes/realTimeProducts.js'
+
+import __dirname from './utils.js'
+
+import ProductManager from './src/daos/product-manager.js'
+
+import { Server } from 'socket.io'
+
+
+
+
+const PORT = 8080
+const server = express()
+server.use(express.json())
+server.use(express.urlencoded({extended:true}))
+
+server.use(express.static(`${__dirname}/public`))
+
+server.engine('handlebars', handlebars.engine())
+server.set('views', __dirname+'/views')
+server.set('view engine', 'handlebars')
+
+server.use('/', homeRouter)
+
+server.use('/', realTimeRouter)
+
+server.use('/api/products', productRouter)
+
+server.use('/api/carts', cartRouter)
+
+
+const httpServer = server.listen(PORT, err =>{
+    if (err)  console.log(err)
+    console.log(`Corriendo en http://localhost:${PORT}`)
+})
+
+const socketServer = new Server(httpServer)
+
+const productManager = new ProductManager(__dirname + '/mockDB/productos.json')
+let history = productManager.getProducts()
+
+socketServer.on('connection', socket => {
+    console.log('Cliente conectado')
+
+    socket.emit('arrayProd', history)
+
+    socket.on('newProduct', (data) => {
+        const {title, description, price, thumbnail, code, stock} = data
+        productManager.addProduct(title, description, price, thumbnail, code, stock)
+        history = productManager.getProducts()
+        socket.emit('arrayProd', history)
+    })
+
+    socket.on('delProduct', (data) => {
+        const { id } = data
+        productManager.deleteProduct(id)
+        history = productManager.getProducts()
+        socket.emit('arrayProd', history)
+    })
+})
